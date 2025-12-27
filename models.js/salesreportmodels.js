88baa11793cccf97f1b070574ac3f2e332d0a2de
Aswin <aswinroyrocks@gmail.com>
2025-12-27 19:18:27 +0530

    Update salesreportmodels.js

--- models.js/salesreportmodels.js
+++ models.js/salesreportmodels.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 
 const salesReportSchema = new mongoose.Schema(
   {
@@ -46,5 +46,69 @@ const salesReportSchema = new mongoose.Schema(
   { timestamps: true, versionKey: false }
 );
 
+module.exports = mongoose.model("salesreport", salesReportSchema);*/
+
+
+
+const mongoose = require('mongoose');
+
+const salesReportSchema = new mongoose.Schema(
+  {
+    billNo: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    customer: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: Number,
+      required: true
+    },
+    mode: {
+      type: String,
+      enum: ["upi", "cash", "card"],
+      required: true
+    },
+
+    total: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    tax: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0
+    },
+
+    paid: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0
+    },
+    pending: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0
+    },
+
+    date: {
+      type: Date,
+      required: true,
+      default: Date.now
+    }
+  },
+  { timestamps: true, versionKey: false }
+);
+
 module.exports = mongoose.model("salesreport", salesReportSchema);
+
+
 
