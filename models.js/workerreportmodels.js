a207877a22646f263582ed8c92af18de11a8a4ef
Aswin <aswinroyrocks@gmail.com>
2025-12-30 01:21:53 +0530

    Update workerreportmodels.js

--- models.js/workerreportmodels.js
+++ models.js/workerreportmodels.js
@@ -1,9 +1,17 @@
-const mongoose= require('mongoose');
-const workerreportSchema =new mongoose.Schema({
-    name:{
-        type:String
+const mongoose = require('mongoose');
+
+const workerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
     },
-    role:{
-        type:String
+    role: {
+      type: String,
+      required: true
     }
-})
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Worker", workerSchema);
