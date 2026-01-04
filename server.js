9126c1183399be936defbe9a2d22adaa2259366f
Aswin <aswinroyrocks@gmail.com>
2026-01-04 20:09:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 //
 app.use(cors({
   origin: [
-    "https://ladybirdboutiquebilling.com/"
+    "https://ladybirdboutiquebilling.com"
   ],
   credentials: true
 }));
@@ -64,6 +64,7 @@ mongoose.connect(MONGO_URL)
 app.listen(PORT, () => {
     console.log(`server is connected successfully on PORT ${PORT}`);
 });
+
 
 
 
